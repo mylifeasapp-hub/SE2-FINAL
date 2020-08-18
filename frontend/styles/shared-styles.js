c3699e0e3e1ad12677f881bced4883a6f1f0ea99
Daniel Prost <daniel.prost@smail.inf.h-brs.de>
2020-08-18 13:26:45 +0200

    initial

---
+++ frontend/styles/shared-styles.js
@@ -0,0 +1,107 @@
+// eagerly import theme styles so as we can override them
+import '@vaadin/vaadin-lumo-styles/all-imports';
+
+import '@vaadin/vaadin-charts/theme/vaadin-chart-default-theme';
+
+const $_documentContainer = document.createElement('template');
+
+$_documentContainer.innerHTML = `
+<custom-style>
+  <style>
+    html {
+      --lumo-font-family: Corbel, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, "Verdana Ref", sans-serif;
+    }
+  </style>
+</custom-style>
+
+<dom-module id="button-style" theme-for="vaadin-button">
+  <template>
+    <style>
+      :host(:not([theme~="tertiary"])) {
+        background-image: linear-gradient(var(--lumo-tint-5pct), var(--lumo-shade-5pct));
+        box-shadow: inset 0 0 0 1px var(--lumo-contrast-20pct);
+      }
+
+      :host(:not([theme~="tertiary"]):not([theme~="primary"]):not([theme~="error"]):not([theme~="success"])) {
+        color: var(--lumo-body-text-color);
+      }
+
+      :host([theme~="primary"]) {
+        text-shadow: 0 -1px 0 var(--lumo-shade-20pct);
+      }
+    </style>
+  </template>
+</dom-module>
+
+
+<custom-style>
+  <style>
+    html {
+      overflow:hidden;
+    }
+  </style>
+</custom-style>
+
+<dom-module id="app-layout" theme-for="vaadin-app-layout">
+  <template>
+    <style>
+      :host(:not([dir='rtl']):not([overlay])) [part='drawer'] {
+        border-right: none;
+        box-shadow: var(--lumo-box-shadow-s);
+        background-color: var(--lumo-base-color);
+        z-index: 1;
+      }
+      :host([dir='rtl']:not([overlay])) [part='drawer'] {
+        border-left: none;
+        box-shadow: var(--lumo-box-shadow-s);
+        background-color: var(--lumo-base-color);
+        z-index: 1;
+      }
+      [part='navbar'] {
+        box-shadow: var(--lumo-box-shadow-s);
+      }
+    </style>
+  </template>
+</dom-module>
+
+<dom-module id="chart" theme-for="vaadin-chart">
+  <template>
+    <style include="vaadin-chart-default-theme">
+      :host {
+        --vaadin-charts-color-0: var(--lumo-primary-color);
+        --vaadin-charts-color-1: var(--lumo-error-color);
+        --vaadin-charts-color-2: var(--lumo-success-color);
+        --vaadin-charts-color-3: var(--lumo-contrast);
+      }
+      .highcharts-container {
+        font-family: var(--lumo-font-family);
+      }
+      .highcharts-background {
+        fill: var(--lumo-base-color);
+      }
+      .highcharts-title {
+        fill: var(--lumo-header-text-color);
+        font-size: var(--lumo-font-size-xl);
+        font-weight: 600;
+        line-height: var(--lumo-line-height-xs);
+      }
+      .highcharts-legend-item text {
+        fill: var(--lumo-body-text-color);
+      }
+      .highcharts-axis-title,
+      .highcharts-axis-labels {
+        fill: var(--lumo-secondary-text-color);
+      }
+      .highcharts-axis-line,
+      .highcharts-grid-line,
+      .highcharts-tick {
+        stroke: var(--lumo-contrast-10pct);
+      }
+      .highcharts-column-series rect.highcharts-point {
+        stroke: var(--lumo-base-color);
+      }
+    </style>
+  </template>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer.content);
